97781ba23aae810b79690b11e1b469f3204657a0
CrisExe <cris.beforus@hotmail.com>
2025-04-22 19:06:25 -0400

    listoooo

--- app/src/app/tabs/tabs.page.ts
+++ app/src/app/tabs/tabs.page.ts
@@ -1,4 +1,6 @@
 import { Component, EnvironmentInjector, inject } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { IonicModule } from '@ionic/angular';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { triangle, ellipse, square } from 'ionicons/icons';
