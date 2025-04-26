3e599e1ebb62bc1744147679c339d1f0930be3f6
CrisExe <cris.beforus@hotmail.com>
2025-04-26 18:02:07 -0400

    inventario

--- Front End/app/src/app/tab1/tab1.page.ts
+++ Front End/app/src/app/tab1/tab1.page.ts
@@ -1,13 +1,56 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
 import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+
+export interface Producto {
+  id: number;
+  nombre: string;
+  cantidad: number;
+  fecha: string;
+  imagen: string;
+}
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, FormsModule, CommonModule, ExploreContainerComponent],
 })
 export class Tab1Page {
   constructor() {}
+  productos: Producto[] = [
+    {
+      id: 1,
+      nombre: 'Doritos',
+      cantidad: 5,
+      fecha: '12/10/2021',
+      imagen: 'assets/images/placeholder.svg',
+    },
+    {
+      id: 2,
+      nombre: 'Bebida',
+      cantidad: 10,
+      fecha: '12/10/2021',
+      imagen: 'assets/images/placeholder.svg',
+    },
+    {
+      id: 3,
+      nombre: 'Bebida 2',
+      cantidad: 7,
+      fecha: '12/10/2021',
+      imagen: 'assets/images/placeholder.svg',
+    },
+  ];
+
+  searchQuery: string = '';
+
+  get filteredProducts() {
+    return this.productos.filter(producto =>
+      producto.nombre.toLowerCase().includes(this.searchQuery.toLowerCase())
+    );
+  }
 }
+
