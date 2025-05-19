7d3d58a8baab189eb74bdf21d59a3de58f08bbcd
CrisExe <cris.beforus@hotmail.com>
2025-05-19 16:17:02 -0400

    arreglo estilo

--- Front End/app/src/app/tabs/tabs.page.ts
+++ Front End/app/src/app/tabs/tabs.page.ts
@@ -3,7 +3,7 @@ import { RouterModule } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
 import { IonTabs, IonTabBar, IonTabButton, IonIcon, IonLabel } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
-import {  ellipse, arrowBack, search, create, add, cubeOutline, cartOutline, barChartOutline, createOutline, trashOutline  } from 'ionicons/icons';
+import {  ellipse, arrowBack, search, create, camera, cubeOutline, cartOutline, barChartOutline, createOutline, trashOutline  } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tabs',
@@ -15,6 +15,6 @@ export class TabsPage {
   public environmentInjector = inject(EnvironmentInjector);
 
   constructor() {
-    addIcons({   arrowBack, search, createOutline, add , cubeOutline ,cartOutline, barChartOutline ,trashOutline});
+    addIcons({   arrowBack, search, createOutline, camera , cubeOutline ,cartOutline, barChartOutline ,trashOutline});
   }
 }
