afe153e04d6bad2dbfcb01f55ac47e9771d22470
CrisExe <cris.beforus@hotmail.com>
2025-06-01 19:34:32 -0400

    cors

--- Back End/express.js
+++ Back End/express.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors({
-  origin: ['http://localhost:8100','capacitor://localhost',"*"], // IP de tu notebook con el frontend
+  origin: ['http://localhost:8100','capacitor://localhost','*'], // IP de tu notebook con el frontend
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }));
