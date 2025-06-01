9907f931d155f86829c1728814afab1130d8120c
Francisco <fr.esteva@duocuc.cl>
2025-05-31 21:09:39 -0400

    crecion modal lista ventas y dashboard para graficos, (hay que quitar el boton de los filtros)

---
+++ Front End/app/src/app/modales/lista-ventas-modal/lista-ventas-modal.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lista-ventas-modal',
+  templateUrl: './lista-ventas-modal.component.html',
+  styleUrls: ['./lista-ventas-modal.component.scss'],
+})
+export class ListaVentasModalComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
